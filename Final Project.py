9edb8c937133273825a7808da4aa8d6cc4a4c5eb
sconwell10 <94002133+sconwell10@users.noreply.github.com>
2023-02-26 23:10:15 -0500

    Add files via upload
    This is the code I have wrote to create a checkers game between two people at the same computer

---
+++ Final Project.py
@@ -0,0 +1,30 @@
+#Seth Conwell
+#SDEV 140
+
+#Create the window for the game
+
+import tkinter as tk
+window = tk.Tk()
+window.geometry("600x600")
+window.config()
+window.resizable()
+window.title('King Me')
+
+#Creat to button to start the game and quit to game
+
+l1 = tk.Label('King Me',20),font='Calibri'
+#program the game board
+
+#program how to move the peices around the board
+
+#program the rules the peices can move and how they can change
+#once they reach the other side of the board
+
+#program how to designate the change in the peices
+
+#program how to take other peices
+
+#program win conditions
+
+#program the end game screen
+
